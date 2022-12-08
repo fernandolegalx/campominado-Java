ffdf9e94e274be76b003b0242519fc3d665c3b29
Nano <116908088+fernandolegalx@users.noreply.github.com>
2022-12-08 14:55:06 -0300

    Update Aplicacao.java

--- src/modelo/Aplicacao.java
+++ src/modelo/Aplicacao.java
@@ -4,7 +4,7 @@ import visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		Tabuleiro tabuleiro = new Tabuleiro(6,5,5);
 		new TabuleiroConsole(tabuleiro);
 		}
 }
